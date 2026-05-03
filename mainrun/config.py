4efe9883f3393409997aca4f64c962d1c41de493
v-wangg <v.wang@berkeley.edu>
2026-05-04 09:51:53 +1000

    sweep(d_model=512, n_head=8, n_layer=8)

--- mainrun/config.py
+++ mainrun/config.py
@@ -5,9 +5,9 @@ class Hyperparameters:
     block_size: int = 128
     batch_size: int = 256
     vocab_size: int = 16_000
-    n_layer: int = 4
-    n_head: int = 6
-    d_model: int = 384
+    n_layer: int = 8
+    n_head: int = 8
+    d_model: int = 512
     dropout: float = 0.0
     lr: float = 2e-3
     adam_betas: tuple = (0.9, 0.95)
